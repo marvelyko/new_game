6e78ca390bd205c7973852f9c1a48f931d67af18
Tengo Pataraia <pataraiamistera@gmail.com>
2025-09-27 16:27:55 +0400

    df

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+const convex = new ConvexReactClient("https://focused-mammoth-966.convex.cloud", {
   unsavedChangesWarning: false,
 });
 
